c66c2e0c2ecd311dbe0462ae0843cfeda38f47f7
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2026-05-21 19:54:55 -0700

    bump go-cli-tools to v1.7.5

--- Formula/go-cli-tools.rb
+++ Formula/go-cli-tools.rb
@@ -1,4 +1,4 @@
-class UgoUcliUtools < Formula
+class GoCliTools < Formula
   desc "A collection of Go CLI tools"
   homepage "https://github.com/Caspian-X/homebrew-tools"
   version "1.7.5"
@@ -7,20 +7,20 @@ class UgoUcliUtools < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_darwin_arm64.tar.gz"
-      sha256 "02af2d501e52937abf20f78e0a7afc85626e316047f5eff112434206ecbe32a4"
+      sha256 "7c39ad0c7922667cbd10851bb014c62c0c4b1cd0558cc8e1af84226eaf4f65d1"
     else
       url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_darwin_amd64.tar.gz"
-      sha256 "bb947a661f7b636a080985db501ff5d4217a9df26a91aa21e31b2cacf6b34fe7"
+      sha256 "b5a449460ceca23dae9f9b9319be8745bacf6cf2e6a20596b7422b1d88d752be"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_linux_arm64.tar.gz"
-      sha256 "fc5d8c792c6c561f9093363720913a1b9c13608addfb2b6add6c92d04e705313"
+      sha256 "bd5907000d319c7b6ce5c5cd89116bdd954fe2836602e70e791c7f9c373789f8"
     else
       url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_linux_amd64.tar.gz"
-      sha256 "6297a288969897ebee87fbb7c1e8f112ca2c07b1fff91c633b1b304b03f2001a"
+      sha256 "8100b457fd56458aba11415e0561bebc88f1b4d706b90559cea8e7b8c93e35e4"
     end
   end
 
