f64df0b182521887cea88377f9ec1f162c26503b
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2026-05-26 16:41:50 -0700

    bump go-cli-tools to v1.7.6

--- Formula/go-cli-tools.rb
+++ Formula/go-cli-tools.rb
@@ -1,26 +1,26 @@
 class GoCliTools < Formula
   desc "A collection of Go CLI tools"
   homepage "https://github.com/Caspian-X/homebrew-tools"
-  version "1.7.5"
+  version "1.7.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_darwin_arm64.tar.gz"
-      sha256 "7c39ad0c7922667cbd10851bb014c62c0c4b1cd0558cc8e1af84226eaf4f65d1"
+      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.6/go-cli-tools_1.7.6_darwin_arm64.tar.gz"
+      sha256 "09307ca6f8081079bf18c19249893cabd9c82e4e44d8246c307ba229325d0cac"
     else
-      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_darwin_amd64.tar.gz"
-      sha256 "b5a449460ceca23dae9f9b9319be8745bacf6cf2e6a20596b7422b1d88d752be"
+      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.6/go-cli-tools_1.7.6_darwin_amd64.tar.gz"
+      sha256 "dc8d060275e1171737acb58f864c97df349385993a75a40d31e7a5d4490209fd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_linux_arm64.tar.gz"
-      sha256 "bd5907000d319c7b6ce5c5cd89116bdd954fe2836602e70e791c7f9c373789f8"
+      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.6/go-cli-tools_1.7.6_linux_arm64.tar.gz"
+      sha256 "7f17cada96f5cfff1ced441f59e66cd0cb2c70db219c117757a0c1803270c817"
     else
-      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.5/go-cli-tools_1.7.5_linux_amd64.tar.gz"
-      sha256 "8100b457fd56458aba11415e0561bebc88f1b4d706b90559cea8e7b8c93e35e4"
+      url "https://github.com/Caspian-X/homebrew-tools/releases/download/v1.7.6/go-cli-tools_1.7.6_linux_amd64.tar.gz"
+      sha256 "4efce93daffa1664e76d30233fbd43ab5ef213839b59bf81e34cada264cdc987"
     end
   end
 
